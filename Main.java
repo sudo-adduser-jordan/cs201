8594f239cdd985c05491dcfa02f72819742e7e92
jordan <sudo.sendmail.jordan@gmail.com>
2024-03-14 09:47:08 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,19 @@ import java.util.Scanner;
 
 public class Main {
 
+    // For the reviewer if this code gets executed.
+
+    // ANSI COLOR Codes
+    public static final String RESET = "\u001B[0m";
+    public static final String BLACK = "\u001B[30m";
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String PURPLE = "\u001B[35m";
+    public static final String CYAN = "\u001B[36m";
+    public static final String WHITE = "\u001B[37m";
+
     public static void main(String[] args) {
         String[][] states_and_captials = {
                 { "alabama", "montgomery" },
@@ -56,40 +69,83 @@ public class Main {
                 { "wisconsin", "madison" },
                 { "wyoming", "cheyenne" }
         };
+        // Integer total = 0;
 
-        TestCapital(); // Test single capital
-        PrintArray(states_and_captials); // Print array
-        // BubbleSort(states_and_captials); // Sort Array
-        PrintArray(states_and_captials); // Print array
-        TestCapitals(); // Test all capitals
-        PrintResults(); // Print results
+        System.out.println(GREEN + "Initial Array: " + RESET);
+        // printArray2D(states_and_captials);
+        prettyPrintArray2D(states_and_captials);
+
+        bubbleSort(states_and_captials);
+
+        System.out.println(GREEN + "Bubble Sorted Array: " + RESET);
+        // printArray2D(states_and_captials);
+        prettyPrintArray2D(states_and_captials);
+        // TestCapitals(); // Test all capitals
+        // PrintResults(); // Print results
     }
 
-    public static void TestCapital() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("*Answers are not case-sensitive.");
-        System.out.println("Enter a United States capital: ");
-        String input = scanner.nextLine();
-        scanner.close();
-        System.out.println("The capital entered: " + input.toLowerCase());
-
-        System.out.println("The value entered does not match a United States capital.");
-        System.out.println("The value entered matches a United States capital.");
+    public static void bubbleSort(String[][] array2D) {
+        int len = array2D.length;
+        boolean swapped;
+        do {
+            swapped = false;
+            for (int row = 1; row < len; row++) { // Compare the capital (second element) of the current element with
+                                                  // the capital of the previous element
+                if (array2D[row - 1][1].compareToIgnoreCase(array2D[row][1]) > 0) { // If the current element is smaller
+                                                                                    // than the previous element, swap
+                                                                                    // them
+                    String[] temp = array2D[row - 1];
+                    array2D[row - 1] = array2D[row];
+                    array2D[row] = temp;
+                    swapped = true; // Indicates that a swap occurred in this pass
+                }
+            }
+        } while (swapped); // Continue iterating and swapping until no more swaps are needed
     }
 
-    public static void TestCapitals() {
-        // for
-        System.out.println("What is the capital of the the state: " + "state_variable" + " ?");
-    }
+    public static void prettyPrintArray2D(String[][] array2D) {
+        System.out.println("[");
+        for (String[] array : array2D) {
 
-    public static void PrintResults() {
-        System.out.println("The total ");
-    }
+            String stringOne = "\t"
+                    + "[ "
+                    + YELLOW
+                    + array[0]
+                    + RESET
+                    + "\t\t";
 
-    public static void PrintArray(String[][] list) {
-        for (String[] item : list) {
-            System.out.println(Arrays.toString(item));
+            String stringTwo = GREEN
+                    + array[1]
+                    + RESET
+                    + "\t]";
+
+            if (array[1].length() <= 7) {
+                stringTwo = GREEN
+                        + array[1]
+                        + RESET
+                        + "\t\t]";
+            }
+
+            if (array[0].length() < 6) {
+                stringOne = "\t"
+                        + "[ "
+                        + YELLOW
+                        + array[0]
+                        + RESET
+                        + "\t\t\t";
+            }
+            if (array[0].length() > 13) {
+                stringOne = "\t"
+                        + "[ "
+                        + YELLOW
+                        + array[0]
+                        + RESET
+                        + "\t";
+            }
+
+            System.out.println(stringOne + stringTwo);
         }
+        System.out.println("]");
     }
 
     public static boolean isValidState(String state, String Capital) {
@@ -99,50 +155,5 @@ public class Main {
     public static boolean isValidCapital(String state, String Capital) {
         return false;
     }
+
 }
-
-// private static int[] BubbleSort(int[] list, int length) {
-// boolean swap = true;
-// int temp;
-// while (swap) {
-// swap = false;
-// for (int i = 0; i < list.length - 1; i++) {
-// if (list[i] > list[i + 1]) {
-// temp = list[i];
-// list[i] = list[i + 1];
-// list[i + 1] = temp;
-// swap = true;
-// }
-// }
-// }
-// return list;
-// }
-
-// String[][] test = {
-// { "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado",
-// "Connecticut", "Delaware",
-// "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa",
-// "Kansas", "Kentucky",
-// "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota",
-// "Mississippi",
-// "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey",
-// "New Mexico",
-// "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon",
-// "Pennsylvania",
-// "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas",
-// "Utah", "Vermont",
-// "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming" },
-// { "Montgomery", "Juneau", "Phoenix", "Little Rock", "Sacramento", "Denver",
-// "Hartford", "Dover",
-// "Tallahassee", "Atlanta", "Honolulu", "Boise", "Springfield", "Indianapolis",
-// "Des Moines",
-// "Topeka", "Frankfort", "Baton Rouge", "Augusta", "Annapolis", "Boston",
-// "Lansing", "St. Paul",
-// "Jackson", "Jefferson City", "Helena", "Lincoln", "Carson City", "Concord",
-// "Trenton",
-// "Santa Fe", "Albany", "Raleigh", "Bismarck", "Columbus", "Oklahoma City",
-// "Salem", "Harrisburg",
-// "Providence", "Columbia", "Pierre", "Nashville", "Austin", "Salt Lake City",
-// "Montpelier",
-// "Richmond", "Olympia", "Charleston", "Madison", "Cheyenne" }
-// };
