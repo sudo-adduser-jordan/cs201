3013371549ca10ba34d184e9a7cf6e04fd0431fb
jordan <sudo.sendmail.jordan@gmail.com>
2024-03-15 08:11:28 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,76 +10,76 @@ import java.util.stream.Collectors;
 public class Main {
 
     // ANSI COLOR Codes
-    public static final String RESET = "\u001B[0m";
-    public static final String BLACK = "\u001B[30m";
-    public static final String RED = "\u001B[31m";
-    public static final String GREEN = "\u001B[32m";
-    public static final String YELLOW = "\u001B[33m";
-    public static final String BLUE = "\u001B[34m";
-    public static final String PURPLE = "\u001B[35m";
-    public static final String CYAN = "\u001B[36m";
-    public static final String WHITE = "\u001B[37m";
+    private static final String RESET = "\u001B[0m";
+    private static final String RED = "\u001B[31m";
+    private static final String GREEN = "\u001B[32m";
+    private static final String YELLOW = "\u001B[33m";
+
+    // States and Capitals 2D array
+    private static String[][] statesAndCapitals = {
+            { "Alabama", "Montgomery" },
+            { "Alaska", "Juneau" },
+            { "Arizona", "Phoenix" },
+            { "Arkansas", "Little Rock" },
+            { "California", "Sacramento" },
+            { "Colorado", "Denver" },
+            { "Connecticut", "Hartford" },
+            { "Delaware", "Dover" },
+            { "Florida", "Tallahassee" },
+            { "Georgia", "Atlanta" },
+            { "Hawaii", "Honolulu" },
+            { "Idaho", "Boise" },
+            { "Illinois", "Springfield" },
+            { "Indiana", "Indianapolis" },
+            { "Iowa", "Des Moines" },
+            { "Kansas", "Topeka" },
+            { "Kentucky", "Frankfort" },
+            { "Louisiana", "Baton Rouge" },
+            { "Maine", "Augusta" },
+            { "Maryland", "Annapolis" },
+            { "Massachusetts", "Boston" },
+            { "Michigan", "Lansing" },
+            { "Minnesota", "Saint Paul" },
+            { "Mississippi", "Jackson" },
+            { "Missouri", "Jefferson City" },
+            { "Montana", "Helena" },
+            { "Nebraska", "Lincoln" },
+            { "Nevada", "Carson City" },
+            { "New Hampshire", "Concord" },
+            { "New Jersey", "Trenton" },
+            { "New Mexico", "Santa Fe" },
+            { "New York", "Albany" },
+            { "North Carolina", "Raleigh" },
+            { "North Dakota", "Bismarck" },
+            { "Ohio", "Columbus" },
+            { "Oklahoma", "Oklahoma City" },
+            { "Oregon", "Salem" },
+            { "Pennsylvania", "Harrisburg" },
+            { "Rhode Island", "Providence" },
+            { "South Carolina", "Columbia" },
+            { "South Dakota", "Pierre" },
+            { "Tennessee", "Nashville" },
+            { "Texas", "Austin" },
+            { "Utah", "Salt Lake City" },
+            { "Vermont", "Montpelier" },
+            { "Virginia", "Richmond" },
+            { "Washington", "Olympia" },
+            { "West Virginia", "Charleston" },
+            { "Wisconsin", "Madison" },
+            { "Wyoming", "Cheyenne" }
+    };
+
+    private static Scanner scanner;
 
     public static void main(String[] args) {
-        String[][] statesAndCapitals = {
-                { "Alabama", "Montgomery" },
-                { "Alaska", "Juneau" },
-                { "Arizona", "Phoenix" },
-                { "Arkansas", "Little Rock" },
-                { "California", "Sacramento" },
-                { "Colorado", "Denver" },
-                { "Connecticut", "Hartford" },
-                { "Delaware", "Dover" },
-                { "Florida", "Tallahassee" },
-                { "Georgia", "Atlanta" },
-                { "Hawaii", "Honolulu" },
-                { "Idaho", "Boise" },
-                { "Illinois", "Springfield" },
-                { "Indiana", "Indianapolis" },
-                { "Iowa", "Des Moines" },
-                { "Kansas", "Topeka" },
-                { "Kentucky", "Frankfort" },
-                { "Louisiana", "Baton Rouge" },
-                { "Maine", "Augusta" },
-                { "Maryland", "Annapolis" },
-                { "Massachusetts", "Boston" },
-                { "Michigan", "Lansing" },
-                { "Minnesota", "Saint Paul" },
-                { "Mississippi", "Jackson" },
-                { "Missouri", "Jefferson City" },
-                { "Montana", "Helena" },
-                { "Nebraska", "Lincoln" },
-                { "Nevada", "Carson City" },
-                { "New Hampshire", "Concord" },
-                { "New Jersey", "Trenton" },
-                { "New Mexico", "Santa Fe" },
-                { "New York", "Albany" },
-                { "North Carolina", "Raleigh" },
-                { "North Dakota", "Bismarck" },
-                { "Ohio", "Columbus" },
-                { "Oklahoma", "Oklahoma City" },
-                { "Oregon", "Salem" },
-                { "Pennsylvania", "Harrisburg" },
-                { "Rhode Island", "Providence" },
-                { "South Carolina", "Columbia" },
-                { "South Dakota", "Pierre" },
-                { "Tennessee", "Nashville" },
-                { "Texas", "Austin" },
-                { "Utah", "Salt Lake City" },
-                { "Vermont", "Montpelier" },
-                { "Virginia", "Richmond" },
-                { "Washington", "Olympia" },
-                { "West Virginia", "Charleston" },
-                { "Wisconsin", "Madison" },
-                { "Wyoming", "Cheyenne" }
-        };
-
-        // partOne(statesAndCapitals);
-        partTwo(statesAndCapitals);
+        scanner = new Scanner(System.in);
+        partOne();
+        partTwo();
+        scanner.close();
     }
 
     // Part 1: Sorting Arrays
-    public static void partOne(String[][] statesAndCapitals) {
+    private static void partOne() {
         System.out.println("\n" + GREEN + "Initial array sorted by state: " + RESET);
         prettyPrintArray2D(statesAndCapitals, false);
 
@@ -91,8 +91,24 @@ public class Main {
         testCapitals(statesAndCapitals);
     }
 
+    // Test user on all 50 capitals
+    private static void testCapitals(String[][] statesAndCapitals) {
+        Integer total = 0;
+
+        System.out.println(YELLOW + "\n**Answers are NOT case sensitive." + RESET);
+        for (String[] row : statesAndCapitals) {
+            System.out.print("\nEnter the capital of " + YELLOW + row[0] + RESET + ": ");
+            String answer = scanner.nextLine().toLowerCase().trim();
+            if (answer.matches(row[1].toLowerCase())) {
+                total = total + 1;
+            }
+        }
+
+        System.out.println("\nTotal correct answers: " + total + " / 50");
+    }
+
     // Part 2: Sorting & Searching HashMap
-    private static void partTwo(String[][] statesAndCapitals) {
+    private static void partTwo() {
         System.out.println("\n" + GREEN + "HashMap: " + RESET);
         HashMap<String, String> hashMap = new HashMap<String, String>(statesAndCapitals.length);
         for (String[] row : statesAndCapitals) {
@@ -112,13 +128,12 @@ public class Main {
 
     // Find state and display capital
     private static void findCaptial(Map<String, String> sortedMap) {
-        Scanner scanner = new Scanner(System.in);
         System.out.println(YELLOW + "\n**Input is NOT case sensitive." + RESET);
         System.out.print("\nEnter a state: ");
-        String input = scanner.nextLine().toLowerCase().trim();
-        scanner.close();
 
-        if (input == "") {
+        String input = scanner.nextLine().toLowerCase().trim();
+
+        if (input.length() == 0) {
             System.out.println(RED + "Capital not found for the state entered. \n" + RESET);
             return;
         }
@@ -134,24 +149,6 @@ public class Main {
         } else {
             System.out.println(RED + "Capital not found for the state entered. \n" + RESET);
         }
-    }
-
-    // Test user on all 50 capitals
-    private static void testCapitals(String[][] statesAndCapitals) {
-        Integer total = 0;
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println(YELLOW + "\n**Answers are NOT case sensitive." + RESET);
-        for (String[] row : statesAndCapitals) {
-            System.out.print("\nEnter the capital of " + YELLOW + row[0] + RESET + ": ");
-            String answer = scanner.nextLine().toLowerCase().trim();
-            if (answer.matches(row[1].toLowerCase())) {
-                total = total + 1;
-            }
-        }
-        scanner.close();
-
-        System.out.println("\nTotal correct answers: " + total + " / 50");
     }
 
     // BubbleSort 2D array of strings
@@ -282,5 +279,4 @@ public class Main {
         }
         System.out.println("]");
     }
-
 }
